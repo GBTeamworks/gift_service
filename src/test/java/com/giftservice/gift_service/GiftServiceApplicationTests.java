65c0f23c155dde30aa573ab17d6aa28751b8d339
BigElmo <bigelmogm@gmail.com>
2022-11-17 00:10:40 +1000

    Create base spring boot project

---
+++ src/test/java/com/giftservice/gift_service/GiftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftservice.gift_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
